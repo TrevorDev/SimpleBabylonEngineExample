fc1cc173f945c53d62df9bdeca0a6c24d6746f98
Trevor Baron <trbaron@microsoft.com>
2019-06-25 15:26:56 -0700

    interface change

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Engine } from '@babylonjs/core/Engines/engine';
-import { EffectCreationTools, EffectRenderer, ChainEffectRenderer } from "./renderer";
-import { Vector2 } from "@babylonjs/core";
+import { EffectCreationTools, EffectWrapper, EffectRenderer } from "./renderer";
+import { Vector2 } from "@babylonjs/core/Maths";
 
 var main = async ()=>{
     var canvas = document.getElementById("testCanvas") as HTMLCanvasElement;
@@ -14,95 +14,82 @@ var main = async ()=>{
     });
 
     // Create first effect
-    var renderImageEffect = await EffectCreationTools.CreateEffectAsync("OffsetImage",
-        {
-            engine: engine,
-            fragmentShader: `
-                #extension GL_OES_standard_derivatives : enable
-        
-                varying vec2 vUV;
-                uniform sampler2D inputImage;
-                uniform vec2 offset;
-                void main(void) {
-                    vec2 coords = vUV;
-                    coords.x += offset.x;
-                    vec3 video = texture2D(inputImage, coords).rgb;
-        
-                    gl_FragColor = vec4(video, 1.0);
-                }
-            `,
-            // Position and scale are required for the vertex shader
-            attributeNames: ["position"],
-            uniformNames: ["scale", "offset"],
-            samplerNames: ["inputImage"]
-        }
-    )
-    // var customTexture2 = await EffectCreationTools.CreateTextureAsync(engine,"https://picsum.photos/id/1035/200/200")
+    var renderImage = new EffectWrapper({
+        engine: engine,
+        fragmentShader: `
+            #extension GL_OES_standard_derivatives : enable
+    
+            varying vec2 vUV;
+            uniform sampler2D inputImage;
+            uniform vec2 offset;
+            void main(void) {
+                vec2 coords = vUV;
+                coords.x += offset.x;
+                vec3 video = texture2D(inputImage, coords).rgb;
+    
+                gl_FragColor = vec4(video, 1.0);
+            }
+        `,
+        // Position and scale are required for the vertex shader
+        attributeNames: ["position"],
+        uniformNames: ["scale", "offset"],
+        samplerNames: ["inputImage"]
+    })
     var customTexture = await EffectCreationTools.CreateTextureAsync(engine,"https://picsum.photos/id/1045/200/200")
+    var scaleValue = new Vector2(1,1)
     var offset = new Vector2(0.5, 0)
     document.onwheel = (e)=>{
         offset.x += e.deltaY/1000
     }
-    var s = new Vector2(1,1)
-    var renderImage = new EffectRenderer(engine, renderImageEffect)
     renderImage.onApplyObservable.add(()=>{
-        renderImageEffect.setTexture('inputImage', customTexture);
-        renderImageEffect.setVector2("offset", offset)
-        renderImageEffect.setVector2("scale", s);
+        renderImage.effect.setTexture('inputImage', customTexture);
+        renderImage.effect.setVector2("offset", offset)
+        renderImage.effect.setVector2("scale", scaleValue);
     })
     
-
     // Create second effect
-    var makeRedEffect = await EffectCreationTools.CreateEffectAsync("makeRed",
-        {
-            engine: engine,
-            fragmentShader: `
-                #extension GL_OES_standard_derivatives : enable
-            
-                varying vec2 vUV;
-                uniform sampler2D textureSampler;
-                uniform vec2 offset;
-                void main(void) {
-                    vec3 inputTexture = texture2D(textureSampler, vUV).rgb;
-                    inputTexture.r = 1.0;
-                    gl_FragColor = vec4(inputTexture, 1.0);
-                }
-            `,
-            attributeNames: ["position"],
-            uniformNames: ["scale"],
-            samplerNames: ["textureSampler"]
-        }
-    )
-    var makeRed = new EffectRenderer(engine, makeRedEffect)
+    var makeRed = new EffectWrapper({
+        engine: engine,
+        fragmentShader: `
+            #extension GL_OES_standard_derivatives : enable
+        
+            varying vec2 vUV;
+            uniform sampler2D textureSampler;
+            uniform vec2 offset;
+            void main(void) {
+                vec3 inputTexture = texture2D(textureSampler, vUV).rgb;
+                inputTexture.r = 1.0;
+                gl_FragColor = vec4(inputTexture, 1.0);
+            }
+        `,
+        attributeNames: ["position"],
+        uniformNames: ["scale"],
+        samplerNames: ["textureSampler"]
+    })
     makeRed.onApplyObservable.add(()=>{
-        // This will be set by the ChainEffectRenderer
-        //  makeRedEffect.setTexture('textureSampler', customTexture2);
-        makeRedEffect.setVector2("offset", offset)
-        makeRedEffect.setVector2("scale", s);
+        // This will be set by the effect renderer
+        //  makeRed.effect.setTexture('textureSampler', customTexture2);
+        makeRed.effect.setVector2("offset", offset)
+        makeRed.effect.setVector2("scale", scaleValue);
     })    
 
-    // Create an effect chain to render effects in order
-    var renderer = new ChainEffectRenderer(engine);
-    renderer.effects = [renderImage, makeRed]
+    // Create an effect chain to render effects in desired order
+    var renderer = new EffectRenderer(engine);
+    var effects = [renderImage, makeRed]
 
     // on key press remove an effect
     document.onkeydown = ()=>{
-        if(renderer.effects.length == 1){
-            renderer.effects = [renderImage, makeRed]
+        if(effects.length == 1){
+            effects = [renderImage, makeRed]
         }else{
-            renderer.effects = [renderImage]
+            effects = [renderImage]
         }
     }
 
     // Render loop
-    renderer.render()
-    var loop = ()=>{
-        window.requestAnimationFrame(()=>{
-            renderer.render()
-            loop()
-        })
-    }
-    loop()
+    engine.runRenderLoop(()=>{
+        renderer.render(effects)
+    })
 }
 main();
 
--- renderer.ts
+++ renderer.ts
@@ -4,33 +4,57 @@ import { VertexBuffer } from '@babylonjs/core/Meshes/buffer';
 import { Effect } from '@babylonjs/core/Materials/effect';
 import { Texture, Nullable, Observable, Viewport } from "@babylonjs/core";
 
-export class ChainEffectRenderer {
-    public effects: Array<EffectRenderer> = []
+export class EffectRenderer {
+    private static vertices = [1, 1, -1, 1, -1, -1, 1, -1];
+    private static indices = [0, 1, 2, 0, 2, 3];
+    public vertexBuffers;
+    public indexBuffer;
     private tmpScreenBuffer:Nullable<Texture> = null
-    public outputTexture:Nullable<Texture> = null
-    constructor(public engine){
-
+    constructor(public engine:Engine){
+        this.vertexBuffers = {
+            [VertexBuffer.PositionKind]: new VertexBuffer(engine, EffectRenderer.vertices, VertexBuffer.PositionKind, false, false, 2),
+        };
+        this.indexBuffer = engine.createIndexBuffer(EffectRenderer.indices);
     }
-    render(){
-        this.effects.forEach((effectRenderer, i)=>{
-            if(this.effects.length > 1 && i != this.effects.length - 1){
+    render(effectWrappers:Array<EffectWrapper>, outputTexture:Nullable<Texture> = null){
+        effectWrappers.forEach((effectWrapper, i)=>{
+            var renderTo = outputTexture;
+            if(effectWrappers.length > 1 && i != effectWrappers.length - 1){
                 if(!this.tmpScreenBuffer){
                     this.tmpScreenBuffer = EffectCreationTools.CreateRenderTargetFrameBuffer(this.engine)
                 }
-                effectRenderer.outputTexture = this.tmpScreenBuffer
+                renderTo = this.tmpScreenBuffer
             }else{
-                effectRenderer.outputTexture = this.outputTexture;
+                renderTo = outputTexture;
             }
             // for any next effect make it's input the output of the previous effect
             if(i !== 0){
-                effectRenderer.effect.onBindObservable.addOnce(()=>{
-                    effectRenderer.effect.setTexture("textureSampler", this.tmpScreenBuffer)
+                effectWrapper.effect.onBindObservable.addOnce(()=>{
+                    effectWrapper.effect.setTexture("textureSampler", this.tmpScreenBuffer)
                 })
             }
-            effectRenderer.render()
+
+            // Reset state
+            this.engine.setViewport(new Viewport(0,0, 1, 1));
+            this.engine.enableEffect(effectWrapper.effect);
+            this.engine.setState(false);
+            this.engine.setDepthBuffer(false);
+            this.engine.setDepthWrite(false);
+    
+            // Bind buffers
+            if(renderTo){
+                this.engine.bindFramebuffer(renderTo.getInternalTexture())
+            }
+            this.engine.bindBuffers(this.vertexBuffers, this.indexBuffer, effectWrapper.effect);
+            effectWrapper.onApplyObservable.notifyObservers({});
+
+            // Render
+            this.engine.drawElementsType(Constants.MATERIAL_TriangleFillMode, 0, 6);
+            if(renderTo){
+                this.engine.unBindFramebuffer(renderTo.getInternalTexture())
+            }
         })
     }
-
     dispose(){
         if(this.tmpScreenBuffer){
             this.tmpScreenBuffer.dispose()
@@ -38,52 +62,18 @@ export class ChainEffectRenderer {
     }
 }
 
-export class EffectRenderer {
-    private quadBuffers = null;
-    public outputTexture:Nullable<Texture> = null
+export class EffectWrapper {
     public onApplyObservable = new Observable<{}>();
-    constructor(public engine:Engine, public effect: Effect){
-        this.quadBuffers = new FullScreenQuadBuffers(engine)
-    }
-
-    render(){
-        // Reset state
-        this.engine.setViewport(new Viewport(0,0, 1, 1));
-        this.engine.enableEffect(this.effect);
-        this.engine.setState(false);
-        this.engine.setDepthBuffer(false);
-        this.engine.setDepthWrite(false);
-
-        // Bind buffers
-        if(this.outputTexture){
-            this.engine.bindFramebuffer(this.outputTexture.getInternalTexture())
-        }
-        this.engine.bindBuffers(this.quadBuffers.vertexBuffers, this.quadBuffers.indexBuffer, this.effect);
-        this.onApplyObservable.notifyObservers({});
-        // Render
-        this.engine.drawElementsType(Constants.MATERIAL_TriangleFillMode, 0, 6);
-        if(this.outputTexture){
-            this.engine.unBindFramebuffer(this.outputTexture.getInternalTexture())
-        }
-    }
-}
-
-export class FullScreenQuadBuffers {
-    private static vertices = [1, 1, -1, 1, -1, -1, 1, -1];
-    private static indices = [0, 1, 2, 0, 2, 3];
-    public vertexBuffers;
-    public indexBuffer;
-
-    constructor(engine){
-        this.vertexBuffers = {
-            [VertexBuffer.PositionKind]: new VertexBuffer(engine, FullScreenQuadBuffers.vertices, VertexBuffer.PositionKind, false, false, 2),
-        };
-        this.indexBuffer = engine.createIndexBuffer(FullScreenQuadBuffers.indices);
+    public effect: Effect
+    private static counter = 0;
+    constructor(creationOptions: {engine:Engine, fragmentShader: string, attributeNames:Array<string>, uniformNames:Array<string>, samplerNames:Array<string>}){
+        var shaderName = "EffectWrapperEffect"+EffectWrapper.counter++;
+        Effect.ShadersStore[shaderName+"FragmentShader"] = creationOptions.fragmentShader
+        this.effect = creationOptions.engine.createEffect({vertex: "postprocess", fragment: shaderName}, creationOptions.attributeNames, creationOptions.uniformNames, creationOptions.samplerNames, "", undefined)   
     }
 }
 
 export class EffectCreationTools {
-    public static counter = 0;
     public static CreateTextureAsync(engine, url):Promise<Texture>{
         return new Promise((res, rej)=>{
             var customTexture = new Texture(url, null);
@@ -94,22 +84,6 @@ export class EffectCreationTools {
                 rej()
             });
         })
-    }
-
-    public static CreateEffectAsync(name, options: {engine:Engine, fragmentShader: string, attributeNames:Array<string>, uniformNames:Array<string>, samplerNames:Array<string>}):Promise<Effect>{
-        return new Promise((res, rej)=>{
-            var shaderName = name+EffectCreationTools.counter++;
-            Effect.ShadersStore[shaderName+"FragmentShader"] = options.fragmentShader
-            var effect:Effect = options.engine.createEffect({vertex: "postprocess", fragment: shaderName}, options.attributeNames, options.uniformNames, options.samplerNames, "", undefined)
-            if(effect.isReady){
-                res(effect)
-            }else{
-                effect.onCompiled = ()=>{
-                    res(effect)
-                }
-            }            
-        })
-        
     }
 
     public static CreateRenderTargetFrameBuffer(engine:Engine){
